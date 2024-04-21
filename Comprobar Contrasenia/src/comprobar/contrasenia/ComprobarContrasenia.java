e7030a54df0b9f751e38ab2bb48760ef7e56a18b
Tedtar25 <167707683+Tedtar25@users.noreply.github.com>
2024-04-21 13:03:53 -0600

    Add files via upload

---
+++ Comprobar Contrasenia/src/comprobar/contrasenia/ComprobarContrasenia.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comprobar.contrasenia;
+import java.util.Scanner;
+/**
+ *
+ * @author Cesar Carrillo
+ */
+public class ComprobarContrasenia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sn=new Scanner(System.in);
+        System.out.print("Usuario: ");
+        String usuario=sn.next();
+        
+    }
+    
+}
